4c78aec886d0dc9dc08d0d5cf287d7d8b165f2a9
layersony <sammaingi5@gmail.com>
2021-07-22 13:24:56 +0300

    location add

--- ghetto/models.py
+++ ghetto/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+class Location(models.Model):
+  location = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.location
 
 class Neighborhood(models.Models):
   name = models.CharField(max_length=200)
-  location = models.CharField(max_length=200)
+  location = models.ForeignKey(Location, on_delete=models.SET_NULL)
   policehelpline = models.IntegerField(null=False, blank=False)
   hospitalhelpline = models.IntegerField(null=False, blank=False)
   occupants = models.IntegerField(default=0)
@@ -22,7 +31,6 @@ class Profile(models.Model):
   def create_user_profile(sender, instance, created, **kwargs):
       if created:
           Profile.objects.create(user=instance)
-
 
   @receiver(post_save, sender=User)
   def save_user_profile(sender, instance, **kwargs):
