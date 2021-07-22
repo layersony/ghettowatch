74ee3ee98b839550d2ef328a3cd03a4b3ffe347a
layersony <sammaingi5@gmail.com>
2021-07-22 15:02:25 +0300

    test for location done

--- ghetto/tests.py
+++ ghetto/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Location, Post, Profile, Neighborhood
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestLocation(TestCase):
+  def setUp(self):
+    self.location = Location.objects.create(location='Machakos')
+
+  def tearDown(self):
+    Location.objects.all().delete()
+
+  def test_isinstance(self):
+    self.assertTrue(isinstance(self.location, Location))
+
+  def save_location(self):
+    self.location2 = Location.objects.create(location='Nairobi')
+    self.assertEqual(len(Location.objects.all()), 2)
