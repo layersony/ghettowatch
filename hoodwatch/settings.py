9170cef2b9c64590c7943c71633817198c89d7a8
layersony <sammaingi5@gmail.com>
2021-07-22 11:24:39 +0300

    3rd party app installed

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -18,6 +18,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ghetto',
+    'bootstrap4',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
