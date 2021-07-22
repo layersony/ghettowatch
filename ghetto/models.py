a0a60b1f4c034e24a010dbcf8b8ebcfcf6e5acb8
layersony <sammaingi5@gmail.com>
2021-07-22 13:10:57 +0300

    neighborhood class model

--- ghetto/models.py
+++ ghetto/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Neighborhood(models.Models):
+  name = models.CharField(max_length=200)
+  location = models.CharField(max_length=200)
+  policehelpline = models.IntegerField(null=False, blank=False)
+  hospitalhelpline = models.IntegerField(null=False, blank=False)
+  occupants = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.name
+
+  
