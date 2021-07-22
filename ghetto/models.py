dc201c4d2ae5f602cca83e025f00aba4611c5fc3
layersony <sammaingi5@gmail.com>
2021-07-22 13:51:38 +0300

    find and update neighborhood done

--- ghetto/models.py
+++ ghetto/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.db.models import Q
 
 class Location(models.Model):
   location = models.CharField(max_length=200)
@@ -28,6 +29,15 @@ class Neighborhood(models.Models):
   @classmethod
   def delete_neigborhood(cls, id):
     cls.objects.filter(id=id).delete()
+
+  @classmethod
+  def find_neigborhood(cls, searchterm):
+    searchresults = cls.objects.filter(Q(name__icontains=searchterm))
+    return searchresults
+
+  def update_neighborhood(cls, id, **kwargs):
+    cls.objects.filter(id=id).update(**kwargs)
+
 class Profile(models.Model):
   username = models.OneToOneField(User, on_delete=models.CASCADE)
   bio = models.TextField()
@@ -55,4 +65,3 @@ class Business(models.Model):
 
   def __str__(self):
     return self.businessname
-    
