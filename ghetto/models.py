26bc2519f1415909caefc7f4347546205a912f8b
layersony <sammaingi5@gmail.com>
2021-07-22 13:24:28 +0300

    profile setup

--- ghetto/models.py
+++ ghetto/models.py
@@ -10,4 +10,20 @@ class Neighborhood(models.Models):
   def __str__(self):
     return self.name
 
-  
+class Profile(models.Model):
+  username = models.OneToOneField(User, on_delete=models.CASCADE)
+  bio = models.TextField()
+  phone = models.IntegerField()
+  profilePic = models.ImageField(upload_to='userProfile/', default='userProfile/test.png')
+  location = models.ForeignKey(Location, on_delete=models.SET_NULL)
+  neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null=False, blank=False)
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+      if created:
+          Profile.objects.create(user=instance)
+
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+      instance.profile.save()
