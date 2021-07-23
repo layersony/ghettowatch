5a7a0ffe68bc2b4ae496897f95a37402d3ff1d25
layersony <sammaingi5@gmail.com>
2021-07-23 19:31:48 +0300

    basic view skeleton

--- ghetto/views.py
+++ ghetto/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Postii, Business, Neighborhood, Profile
+from django.contrib.auth.models import User
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def landing(request):
+  return render(request, 'profile/landing.html')
+
+def profile(request):
+  return render(request, 'profile/profile.html')
+
+def search(request):
+  return render(request, 'search.html')
