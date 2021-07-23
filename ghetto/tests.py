9f5c02a0d89b408331750d35eda81899c295bd92
layersony <sammaingi5@gmail.com>
2021-07-23 19:02:29 +0300

    post test add ininstance

--- ghetto/tests.py
+++ ghetto/tests.py
@@ -95,3 +95,18 @@ class TestBusiness(TestCase):
     searchresults = Business.searchbusiness(searchterm)
     self.assertEqual(len(searchresults), 1)
 
+
+class Post(TestCase):
+  def setUp(self):
+    self.location = Location.objects.create(location='Machakos')
+    self.hood = Neighborhood.objects.create(name='Kisumu Ndogo', location=self.location, policehelpline='00022233211', hospitalhelpline='0092133313', occupants=8)
+    self.post = Postii.objects.create(posttitle='theives found', story='wezi wa tatu walipatikana', neighborhood=self.hood)
+
+  def tearDown(self):
+    Location.objects.all().delete()
+    User.objects.all().delete()
+    Neighborhood.objects.all().delete()
+    Postii.objects.all().delete
+
+  def test_isinstance(self):
+    self.assertTrue(isinstance(self.post, Postii))
