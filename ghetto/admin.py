55f6961e1975c81da4b7e17048afb9aff83fac73
layersony <sammaingi5@gmail.com>
2021-07-24 11:10:22 +0300

    admin models add

--- ghetto/admin.py
+++ ghetto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Neighborhood)
+admin.site.register(models.Location)
+admin.site.register(models.Profile)
+admin.site.register(models.Business)
+admin.site.register(models.Postii)
