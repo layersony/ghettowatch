d7644cb092f2ef9658e146289494787282aa7dad
layersony <sammaingi5@gmail.com>
2021-07-24 11:13:18 +0300

    profile forms add

--- ghetto/views.py
+++ ghetto/views.py
@@ -23,8 +23,37 @@ def index(request):
         return render(request, 'index.html', params)
 
 def profile(request):
-  return render(request, 'profile/profile.html')
+    if request.method == 'POST':
+        userform = UserForm(request.POST, instance=request.user)
+        profileform = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        businessform = BusinessForm(request.POST)
+        postiiform = PostiiForm(request.POST)
 
+        curr_neighbourhood = request.user.profile.neighborhood
+
+        if userform.is_valid and profileform.is_valid():
+            userform.save()
+            profileform.save()
+            messages.success(request, 'Profile updated successfully')
+
+        if businessform.is_valid():
+            busi = businessform.save(commit=False)
+            busi.username = request.user
+            busi.neighborhood = request.user.profile.neighborhood
+            busi.save()
+
+        if postiiform.is_valid():
+            post = postiiform.save(commit=False)
+            post.postuser = request.user
+            post.neighborhood = curr_neighbourhood
+            post.save()
+
+        return redirect('uprofile')
+
+    userform = UserForm()
+    profileform = ProfileForm()
+    businessform = BusinessForm()
+    postiiform = PostiiForm()
 @login_required(login_url='/accounts/login/')
 def search(request):
     if 'search' in request.GET and request.GET['search']:
